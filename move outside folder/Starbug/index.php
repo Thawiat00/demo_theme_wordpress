b40c247631307b21c7bb31bb954fecdc532ca8df
Thawiat00 <Thawiat01@gmail.com>
2024-10-06 14:09:41 +0700

    add asset
    asset test workshop

---
+++ move outside folder/Starbug/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link rel="stylesheet" href="css/vendor/vendor.css">k
+	</head>
+	<body>
+		<h1>Hello</h1>
+		<p>Thanks for using our starter theme!</p>
+		
+	</body>
+</html>
